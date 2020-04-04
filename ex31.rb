41cd12d0e4b395a44e742bb4ed96fba333eb73b6
askomar <komarovsky.andrey.prog@gmail.com>
2020-04-04 23:13:59 +0300

    Completed exercise 31

--- ex31.rb
+++ ex31.rb
@@ -2,7 +2,7 @@ def prompt
   print "> "
 end
 
-puts "You enter a dark room whit two doors/ Do you go throught door #1 or door #2?"
+puts "You enter a dark room whit two doors/ Do you go throught door #1 or door #2? or door #3? or others?"
 
 prompt; door = gets.chomp
 
@@ -32,6 +32,28 @@ elsif door == "2"
   else
     puts "The insanity rots your eyes into a pool of muck. Good job!"
   end
+elsif door == "3"
+  puts "You see how bethoven eats his notebook. What do you do?"
+  puts "1. Run away!"
+  puts "2. WAKE UP JONNY"
+  prompt; bethoven = gets.chomp
+  if bethoven == "1"
+    puts "Gradulations! You are in matrix"
+  elsif bethoven == "2"
+    puts "Oooh. Its just a dream...Or no?"
+  else
+    puts "Tuk.tuk.. Wake up, Neo"
+  end
+elsif door == "13"
+  puts "Welcome to the #{door} home! You will stay here for a long time."
+
+elsif door == "666"
+  puts "Hi? my name is Satan and YOU ARE WELCOME TO HELL, HAHAHAAHAxxoooOOox"
+
+elsif door == "1000000000000000"
+  puts "Do you think you are a normal if you find this?"
+elsif door == "0"
+  puts "Congratulates! Let's start this game $)"
 else
   puts "You stumble around and fall on a knife and die. Good job!"
 end
